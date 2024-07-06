012ff050311d4dd7d4920cfa14ee6303fb10b2fe
RikuGhi <rifkhairkhairur@gmail.com>
2024-07-06 09:59:52 +0700

    update

--- services/order.go
+++ services/order.go
@@ -1,17 +1,23 @@
 package services
 
 import (
+	"context"
 	"log"
 
 	"github.com/google/uuid"
+	"github.com/rikughi/go-ddd/aggregate"
 	"github.com/rikughi/go-ddd/domain/customer"
 	"github.com/rikughi/go-ddd/domain/customer/memory"
+	"github.com/rikughi/go-ddd/domain/customer/mongo"
+	"github.com/rikughi/go-ddd/domain/product"
+	prodmem "github.com/rikughi/go-ddd/domain/product/memory"
 )
 
 type OrderConfiguration func(os *OrderService) error
 
 type OrderService struct {
 	customers customer.CustomerRepository
+	products  product.ProductRepository
 }
 
 func NewOrderService(cfgs ...OrderConfiguration) (*OrderService, error) {
@@ -39,12 +45,52 @@ func WithMemoryCustomerRepository() OrderConfiguration {
 	return WithCustomerRepository(cr)
 }
 
-func (o *OrderService) CreateOrder(customerID uuid.UUID, productsIDs []uuid.UUID) error {
+func WithMongoCustomerRepository(connectionString string) OrderConfiguration {
+	return func(os *OrderService) error {
+		// Create the mongo repo, if we needed parameters, such as connection strings they could be inputted here
+		cr, err := mongo.New(context.Background(), connectionString)
+		if err != nil {
+			return err
+		}
+		os.customers = cr
+		return nil
+	}
+}
+
+func WithMemoryProductRepository(products []aggregate.Product) OrderConfiguration {
+	return func(os *OrderService) error {
+		pr := prodmem.New()
+
+		for _, p := range products {
+			if err := pr.Add(p); err != nil {
+				return err
+			}
+		}
+
+		os.products = pr
+		return nil
+	}
+}
+
+func (o *OrderService) CreateOrder(customerID uuid.UUID, productsIDs []uuid.UUID) (float64, error) {
 	c, err := o.customers.Get(customerID)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	log.Println(c)
-	return nil
+	var products []aggregate.Product
+	var price float64
+
+	for _, id := range productsIDs {
+		p, err := o.products.GetByID(id)
+
+		if err != nil {
+			return 0, err
+		}
+
+		products = append(products, p)
+		price += p.GetPrice()
+	}
+	log.Printf("Customer: %s has orderd %d products", c.GetID(), len(products))
+	return price, nil
 }
